bb918490bb812bf9cf21de53fb135b2bd6f3e167
norouzimohammadreza <rda0044@gmail.com>
2025-11-09 12:42:21 +0330

    #1 feat(dua): create dua model and migration

---
+++ src/app/Models/Dua.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Dua extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = [
+        'number',
+        'title',
+        'description',
+    ];
+}
---
+++ src/database/migrations/2025_11_09_085105_create_duas_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('duas', function (Blueprint $table) {
+            $table->mediumIncrements('id');
+            $table->unsignedSmallInteger('number')->unique();
+            $table->string('title', 120)->index();
+            $table->string('description', 300)->index()->nullable();
+
+            $table->timestamps();
+            $table->softDeletes()->index();
+
+            $table->index(['created_at']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('duas');
+    }
+};
