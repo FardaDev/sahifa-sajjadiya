ee1ed234c0656a2469596b7c52826832245fb06c
norouzimohammadreza <rda0044@gmail.com>
2025-11-09 13:23:17 +0330

    #1 feat(dua): create dua resource

---
+++ src/app/Filament/Resources/Duas/Pages/CreateDua.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\Duas\Pages;
+
+use App\Filament\Resources\Duas\DuaResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateDua extends CreateRecord
+{
+    protected static string $resource = DuaResource::class;
+}
---
+++ src/app/Filament/Resources/Duas/Tables/DuasTable.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Filament\Resources\Duas\Tables;
+
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteAction;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Actions\ForceDeleteBulkAction;
+use Filament\Actions\RestoreBulkAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Filters\TrashedFilter;
+use Filament\Tables\Table;
+
+class DuasTable
+{
+    public static function configure(Table $table): Table
+    {
+        return $table
+            ->defaultSort('created_at', 'desc')
+            ->columns([
+                TextColumn::make('number')
+                    ->label(trans('dua.number'))
+                    ->sortable(),
+
+                TextColumn::make('title')
+                    ->label(trans('dua.title'))
+                    ->searchable()
+                    ->sortable(),
+
+                TextColumn::make('description')
+                    ->label(trans('dua.description'))
+                    ->searchable()
+                    ->sortable(),
+            ])
+            ->filters([
+                TrashedFilter::make(),
+            ])
+            ->recordActions([
+                EditAction::make(),
+                DeleteAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                    ForceDeleteBulkAction::make(),
+                    RestoreBulkAction::make(),
+                ]),
+            ]);
+    }
+}
