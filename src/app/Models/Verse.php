78265552c762d6e1ae41f13e74baf50dc79b041b
norouzimohammadreza <rda0044@gmail.com>
2025-11-09 12:43:20 +0330

    #1 feat(verse): create verse model and migration

---
+++ src/app/Models/Verse.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Verse extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = [
+        'dua_id',
+        'text',
+        'translation',
+        'order',
+    ];
+}
---
+++ src/database/migrations/2025_11_09_090552_create_verses_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('verses', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedMediumInteger('dua_id')->index();
+            $table->unsignedMediumInteger('order')->index();
+            $table->text('text');
+            $table->text('translation')->nullable();
+
+            $table->timestamps();
+            $table->softDeletes()->index();
+
+            $table->index(['created_at']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('verses');
+    }
+};
